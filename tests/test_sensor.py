d39c724d4613e3a440c3270f61c08d3b27dd426f
Joshua Spence <josh@spence.com.au>
2022-03-15 11:46:04 +1100

    Add a basic test

---
+++ tests/test_sensor.py
@@ -0,0 +1,11 @@
+from custom_components.unifi_network.sensor import DEFAULT_NAME, SENSOR_WWW, UnifiNetworkSensor
+from pytest_homeassistant_custom_component.common import MockConfigEntry
+from unittest.mock import MagicMock
+
+async def test_sensor(hass):
+    controller = MagicMock()
+
+    sensor = UnifiNetworkSensor(hass, controller, DEFAULT_NAME, SENSOR_WWW)
+    sensor.update()
+
+    assert None == sensor.state
