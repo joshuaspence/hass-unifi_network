67239bf805f0c803b8bc0b378142b5ad617adf2e
Joshua Spence <josh@spence.com.au>
2022-03-16 09:10:34 +1100

    Add some more tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,7 +1,15 @@
+from unittest.mock import MagicMock
+
 import pytest
+from pyunifi.controller import Controller
 
 
 @pytest.fixture(autouse=True)
 def auto_enable_custom_integrations(enable_custom_integrations):  # type: ignore[no-untyped-def]
     """Enable custom integrations in all tests."""
     yield
+
+
+@pytest.fixture
+def controller() -> Controller:
+    return MagicMock()
--- tests/test_sensor.py
+++ tests/test_sensor.py
@@ -1,16 +1,26 @@
-from unittest.mock import MagicMock
+from homeassistant.core import HomeAssistant
+from pyunifi.controller import Controller
 
 from custom_components.unifi_network.sensor import (
     DEFAULT_NAME,
     SENSOR_WWW,
+    SENSORS,
     UnifiNetworkSensor,
 )
 
 
-async def test_sensor(hass):  # type: ignore[no-untyped-def]
-    controller = MagicMock()
-
+async def test_sensor(hass: HomeAssistant, controller: Controller) -> None:
     sensor = UnifiNetworkSensor(hass, controller, DEFAULT_NAME, SENSOR_WWW)
     sensor.update()
 
     assert sensor.state is None
+
+
+async def test_sensor_properties(hass: HomeAssistant, controller: Controller) -> None:
+    type = SENSOR_WWW
+    name = "Sensor Name"
+
+    sensor = UnifiNetworkSensor(hass, controller, name, type)
+
+    assert sensor.name == name + " " + SENSORS[type][0]
+    assert sensor.icon == SENSORS[type][2]
